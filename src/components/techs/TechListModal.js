82247c7d7193e47fef37facee8c086c88f84b3ed
abdu020 <you@example.com>
2019-08-08 14:29:12 +0200

    first commit

---
+++ src/components/techs/TechListModal.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import TechItem from "./TechItem";
+
+const TechListModal = () => {
+  const [techs, setTechs] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getTechs();
+    //eslint-disable-next-line
+  }, []);
+
+  const getTechs = async () => {
+    setLoading(true);
+
+    /* the techs in fetch comes from th db.json
+       and the reason we only need that is because
+       we set up a proxy in package.json so we don't
+       have to write out the whole localhost address. */
+    const res = await fetch("/techs");
+    const data = await res.json();
+
+    setTechs(data);
+    setLoading(false);
+  };
+
+  return (
+    <div id="tech-list-modal" className="modal">
+      <div className="modal-content">
+        <h4>Technician list</h4>
+        <ul className="collection">
+          {!loading &&
+            techs.map(tech => <TechItem tech={tech} key={tech.id} />)}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default TechListModal;
